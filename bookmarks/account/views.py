aa1e9393628f2d8f80bdcaacc9413af9c9227482
zhuravlevivan <855434@gmail.com>
2024-01-20 14:29:35 +0300

    20.01.2024

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
